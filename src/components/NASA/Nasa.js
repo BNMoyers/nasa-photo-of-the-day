0f0d0c5ad1734f3ddc16fd7ae80ba627ca445929
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-17 17:20:40 -0500

    update

--- src/components/NASA/Nasa.js
+++ src/components/NASA/Nasa.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect} from 'react';
-
+import axios from 'axios';
 
 
 
 
 const SpacePic = () =>{
     const [dailyPic, setDailyPic] = useState("");
-
+    useEffect(() => {
+        axios
+            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    }, []);
     return (
         <img src={dailyPic} alt = "Space - the Final Frontier."/>
     );
